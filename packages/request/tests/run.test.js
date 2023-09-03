55728865feb892493492df016a57e9f5b430b425
TyDoesDev <tydoesdev@gmail.com>
2023-09-03 02:19:05 -0600

    remove tests on working packages

--- packages/request/tests/run.test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const { get_method } = require('./workers/get.test');
-const wait = (time) => new Promise(resolve => setTimeout(() => resolve(), time)); 
-
-async function startTests() {
-    await get_method();
-}
-
-startTests();
