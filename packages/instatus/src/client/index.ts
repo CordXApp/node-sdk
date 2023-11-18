e6b74947cef247b98d84dc872bbfef1ec5485093
TheRealToxicDev <toxic.dev09@gmail.com>
2023-11-18 02:22:37 -0700

    feat(add): lerna fixes

--- packages/instatus/src/client/index.ts
+++ packages/instatus/src/client/index.ts
@@ -1,11 +1,11 @@
-import * as User from '../typings/user';
-import * as Page from '../typings/page';
-import * as Subscribers from '../typings/subscribers';
-import * as Incidents from '../typings/incidents';
-import * as Components from '../typings/components';
-import { InstatusOptions } from '../typings/defaults';
-import { EventEmitter } from 'events';
-import axios from 'axios';
+import * as User from '../typings/user'
+import * as Page from '../typings/page'
+import * as Subscribers from '../typings/subscribers'
+import * as Incidents from '../typings/incidents'
+import * as Components from '../typings/components'
+import { InstatusOptions } from '../typings/defaults'
+import { EventEmitter } from 'events'
+import axios from 'axios'
 
 /**
  * base client for all of the get methods/endpoints
@@ -13,45 +13,45 @@ import axios from 'axios';
  * @extends EventEmitter
  */
 export class InstatusClient extends EventEmitter {
-	private _opts: InstatusOptions;
-	private _defaultURL: string;
-	private _defaultVersion: string;
-	private _apiKey: string;
-	private _apiVersion: string;
-	private _pageId: string;
-	private _httpClient: any;
+    private _opts: InstatusOptions
+    private _defaultURL: string
+    private _defaultVersion: string
+    private _apiKey: string
+    private _apiVersion: string
+    private _pageId: string
+    private _httpClient: any
 
-	public _disclaimer: string;
-	public _copyright: string;
-	public _docs: string;
+    public _disclaimer: string
+    public _copyright: string
+    public _docs: string
 
-	constructor(options: InstatusOptions) {
-		super();
+    constructor(options: InstatusOptions) {
+        super()
 
-		this._defaultURL = 'https://api.instatus.com';
-		this._defaultVersion = 'v2';
+        this._defaultURL = 'https://api.instatus.com'
+        this._defaultVersion = 'v2'
 
-		if (!options.apiKey) {
-			throw new ReferenceError('[@cordxapp/instatus]: Client should have a valid instatus api key');
-		}
+        if (!options.apiKey) {
+            throw new ReferenceError('[@cordxapp/instatus]: Client should have a valid instatus api key')
+        }
 
-		this._opts = options;
-		this._apiKey = options.apiKey;
-		this._apiVersion = options.apiVersion || this._defaultVersion;
-		this._pageId = options.pageId;
-		this._copyright = '© 2023 - Infinity Development';
-		this._disclaimer = '"@cordxapp/instatus" is not owned, endorsed by or affiliated with https://instatus.com!';
-		this._docs = 'https://cordxapp.github.io/node-sdk/modules/instatus_src.html';
+        this._opts = options
+        this._apiKey = options.apiKey
+        this._apiVersion = options.apiVersion || this._defaultVersion
+        this._pageId = options.pageId
+        this._copyright = '© 2023 - Infinity Development'
+        this._disclaimer = '"@cordxapp/instatus" is not owned, endorsed by or affiliated with https://instatus.com!'
+        this._docs = 'https://cordxapp.github.io/node-sdk/modules/instatus_src.html'
 
-		this._httpClient = axios.create({
-			baseURL: `${this._defaultURL}/${this._apiVersion}`,
-			headers: {
-				Authorization: `Bearer ${this._apiKey}`,
-			},
-		});
-	}
+        this._httpClient = axios.create({
+            baseURL: `${this._defaultURL}/${this._apiVersion}`,
+            headers: {
+                Authorization: `Bearer ${this._apiKey}`
+            }
+        })
+    }
 
-	/**
+    /**
      * VIEW YOUR PUBLIC PROFILE INFORMATION VIA YOUR API KEY
      * @interface UserProfile user profile typings
      * @returns instatus profile information
@@ -65,15 +65,15 @@ export class InstatusClient extends EventEmitter {
      *
      * instatus.getUserProfile();
      */
-	public async getUserProfile(): Promise<User.UserProfile> {
-		const res = await this._httpClient.get('/user');
+    public async getUserProfile(): Promise<User.UserProfile> {
+        const res = await this._httpClient.get('/user')
 
-		if (res.data.error) throw new Error(`[@cordxapp/instatus:error]: ${res.data.error}`);
+        if (res.data.error) throw new Error(`[@cordxapp/instatus:error]: ${res.data.error}`)
 
-		return res.data;
-	}
+        return res.data
+    }
 
-	/**
+    /**
      * VIEW AN ARRAY OF THE STATUS PAGES IN YOUR TEAM/WORKSPACE
      * @interface Page.Information status page typings
      * @returns array of status pages in your team/workspace
@@ -87,15 +87,15 @@ export class InstatusClient extends EventEmitter {
      *
      * instatus.getStatusPages();
      */
-	public async getStatusPages(): Promise<Page.Information[]> {
-		const res = await this._httpClient.get('/pages');
+    public async getStatusPages(): Promise<Page.Information[]> {
+        const res = await this._httpClient.get('/pages')
 
-		if (res.data.error) throw new Error(`[@cordxapp/instatus:error]: ${res.data.error}`);
+        if (res.data.error) throw new Error(`[@cordxapp/instatus:error]: ${res.data.error}`)
 
-		return res.data;
-	}
+        return res.data
+    }
 
-	/**
+    /**
      * VIEW A QUERIED ARRAY OF THE STATUS PAGES IN YOUR TEAM/WORKSPACE
      * @interface Page.Information status page typings
      * @returns array of status pages in your team/workspace
@@ -109,30 +109,27 @@ export class InstatusClient extends EventEmitter {
      *
      * instatus.getQueriedStatusPages({ page: 1, per_page: 20 });
      */
-	public async getQueriedStatusPages(query: { page: Page.Query; per_page: Page.Query }): Promise<Page.Information[]> {
-		if (!query || query == undefined) {
-			throw new ReferenceError('[@cordxapp/instatus:error]: please provide a valid query');
-		}
-		else if (typeof query.page !== 'number') {
-			throw new TypeError('[@cordxapp/instatus:error]: page param should be a valid number');
-		}
-		else if (typeof query.per_page !== 'number') {
-			throw new TypeError('[@cordxapp/instatus:error]: page param should be a valid number');
-		}
-		else if (typeof query !== 'object') {
-			throw new TypeError(
-				'[@cordxapp/instatus:error]: query should be a valid object | Ex: getQueriedIncidents({ page, per_page })'
-			);
-		}
+    public async getQueriedStatusPages(query: { page: Page.Query; per_page: Page.Query }): Promise<Page.Information[]> {
+        if (!query || query == undefined) {
+            throw new ReferenceError('[@cordxapp/instatus:error]: please provide a valid query')
+        } else if (typeof query.page !== 'number') {
+            throw new TypeError('[@cordxapp/instatus:error]: page param should be a valid number')
+        } else if (typeof query.per_page !== 'number') {
+            throw new TypeError('[@cordxapp/instatus:error]: page param should be a valid number')
+        } else if (typeof query !== 'object') {
+            throw new TypeError(
+                '[@cordxapp/instatus:error]: query should be a valid object | Ex: getQueriedIncidents({ page, per_page })'
+            )
+        }
 
-		const res = await this._httpClient.get(`/pages?page=${query.page}&per_page=${query.per_page}`);
+        const res = await this._httpClient.get(`/pages?page=${query.page}&per_page=${query.per_page}`)
 
-		if (res.data.error) throw new Error(`[@cordxapp/instatus:error]: ${res.data.error}`);
+        if (res.data.error) throw new Error(`[@cordxapp/instatus:error]: ${res.data.error}`)
 
-		return res.data;
-	}
+        return res.data
+    }
 
-	/**
+    /**
      * VIEW AN ARRAY OF ALL THE COMPONENTS IN YOUR STATUS PAGE
      * @requires InstatusOptions.pageId
      * @returns array of status page components
@@ -147,21 +144,21 @@ export class InstatusClient extends EventEmitter {
      *
      * instatus.getComponent({ id: 'some_comp_id' });
      */
-	public async getComponents(pageId: InstatusOptions): Promise<Page.Components[]> {
-		if (!this._pageId) {
-			throw new ReferenceError(
-				'[@cordxapp/instatus:error]: please provide a valid page id in the client options to use this method'
-			);
-		}
+    public async getComponents(pageId: InstatusOptions): Promise<Page.Components[]> {
+        if (!this._pageId) {
+            throw new ReferenceError(
+                '[@cordxapp/instatus:error]: please provide a valid page id in the client options to use this method'
+            )
+        }
 
-		const res = await this._httpClient.get(`/${pageId}/components`);
+        const res = await this._httpClient.get(`/${pageId}/components`)
 
-		if (res.data.error) throw new Error(`[@cordxapp/instatus:error]: ${res.data.error}`);
+        if (res.data.error) throw new Error(`[@cordxapp/instatus:error]: ${res.data.error}`)
 
-		return res.data;
-	}
+        return res.data
+    }
 
-	/**
+    /**
      * GET A STATUS PAGE COMPONENT BY ID
      * @param id the component id
      * @requires id
@@ -176,20 +173,20 @@ export class InstatusClient extends EventEmitter {
      *
      * instatus.getComponent({ id: 'some_component_id' });
      */
-	public async getComponent({ id }: Components.Fetch): Promise<Page.Components> {
-		if (!id) throw new ReferenceError('[@cordxapp/instatus:error]: please provide a component id');
-		if (typeof id !== 'string') {
-			throw new TypeError('[@cordxapp/instatus:error]: component id should be a valid string');
-		}
+    public async getComponent({ id }: Components.Fetch): Promise<Page.Components> {
+        if (!id) throw new ReferenceError('[@cordxapp/instatus:error]: please provide a component id')
+        if (typeof id !== 'string') {
+            throw new TypeError('[@cordxapp/instatus:error]: component id should be a valid string')
+        }
 
-		const res = await this._httpClient.get(`/${this._pageId}/components/${id}`);
+        const res = await this._httpClient.get(`/${this._pageId}/components/${id}`)
 
-		if (res.data.error) throw new Error(`[@cordxapp/instatus:error]: ${res.data.error}`);
+        if (res.data.error) throw new Error(`[@cordxapp/instatus:error]: ${res.data.error}`)
 
-		return res.data;
-	}
+        return res.data
+    }
 
-	/**
+    /**
      * GET A LIST OF INCIDENTS
      * @interface BaseIncident incident typings
      * @returns array of incidents
@@ -204,15 +201,15 @@ export class InstatusClient extends EventEmitter {
      *
      * instatus.getIncidents();
      */
-	public async getIncidents(): Promise<Incidents.Base[]> {
-		const res = await this._httpClient.get(`/${this._pageId}/incidents`);
+    public async getIncidents(): Promise<Incidents.Base[]> {
+        const res = await this._httpClient.get(`/${this._pageId}/incidents`)
 
-		if (res.data.error) throw new Error(`[@cordxapp/instatus:error]: ${res.data.error}`);
+        if (res.data.error) throw new Error(`[@cordxapp/instatus:error]: ${res.data.error}`)
 
-		return res.data;
-	}
+        return res.data
+    }
 
-	/**
+    /**
      * GET A QUERIED LIST OF INCIDENTS
      * @interface BaseIncident
      * @returns an array of incidents
@@ -230,75 +227,69 @@ export class InstatusClient extends EventEmitter {
      *     per_page: 20
      * });
      */
-	public async getQueriedIncidents(query?: {
+    public async getQueriedIncidents(query?: {
         page: Incidents.Query
         per_page: Incidents.Query
     }): Promise<Incidents.Base[]> {
-		if (!query || query == undefined) {
-			throw new ReferenceError('[@cordxapp/instatus:error]: please provide a valid query');
-		}
-		else if (typeof query.page !== 'number') {
-			throw new TypeError('[@cordxapp/instatus:error]: page param should be a valid number');
-		}
-		else if (typeof query.per_page !== 'number') {
-			throw new TypeError('[@cordxapp/instatus:error]: page param should be a valid number');
-		}
-		else if (typeof query !== 'object') {
-			throw new TypeError(
-				'[@cordxapp/instatus:error]: query should be a valid object | Ex: getQueriedIncidents({ page, per_page })'
-			);
-		}
+        if (!query || query == undefined) {
+            throw new ReferenceError('[@cordxapp/instatus:error]: please provide a valid query')
+        } else if (typeof query.page !== 'number') {
+            throw new TypeError('[@cordxapp/instatus:error]: page param should be a valid number')
+        } else if (typeof query.per_page !== 'number') {
+            throw new TypeError('[@cordxapp/instatus:error]: page param should be a valid number')
+        } else if (typeof query !== 'object') {
+            throw new TypeError(
+                '[@cordxapp/instatus:error]: query should be a valid object | Ex: getQueriedIncidents({ page, per_page })'
+            )
+        }
 
-		const res = await this._httpClient.get(
-			`/${this._pageId}/incidents?page=${query.page}&per_page=${query.per_page}`
-		);
+        const res = await this._httpClient.get(
+            `/${this._pageId}/incidents?page=${query.page}&per_page=${query.per_page}`
+        )
 
-		if (res.data.error) throw new Error(`[@cordxapp/instatus:error]: ${res.data.error}`);
+        if (res.data.error) throw new Error(`[@cordxapp/instatus:error]: ${res.data.error}`)
 
-		return res.data;
-	}
+        return res.data
+    }
 
-	public async getIncident({ id }: Incidents.Base): Promise<Incidents.Base> {
-		if (!id) throw new ReferenceError('[@cordxapp/instatus:error]: please provide a valid incident id');
-		if (typeof id !== 'string') {
-			throw new TypeError('[@cordxapp/instatus:error]: incident id should be a valid string');
-		}
+    public async getIncident({ id }: Incidents.Base): Promise<Incidents.Base> {
+        if (!id) throw new ReferenceError('[@cordxapp/instatus:error]: please provide a valid incident id')
+        if (typeof id !== 'string') {
+            throw new TypeError('[@cordxapp/instatus:error]: incident id should be a valid string')
+        }
 
-		const res = await this._httpClient.get(`${this._pageId}/incidents/${id}`);
+        const res = await this._httpClient.get(`${this._pageId}/incidents/${id}`)
 
-		if (res.data.error) throw new Error(`[@cordxapp/instatus:error]: ${res.data.error}`);
+        if (res.data.error) throw new Error(`[@cordxapp/instatus:error]: ${res.data.error}`)
 
-		return res.data;
-	}
+        return res.data
+    }
 
-	/**
+    /**
      * VIEW A LIST OF PEOPLE SUBSCRIBED TO YOUR STATUS PAGE
      * @param query
      * @returns array of status page subscribers
      */
-	public async getSubscribers(query?: {
+    public async getSubscribers(query?: {
         page: Subscribers.Query
         per_page: Subscribers.Query
     }): Promise<Subscribers.Base> {
-		if (!query || query == undefined) {
-			throw new ReferenceError('[@cordxapp/instatus:error]: please provide a valid query');
-		}
-		else if (typeof query.page !== 'number') {
-			throw new TypeError('[@cordxapp/instatus:error]: page param should be a valid number');
-		}
-		else if (typeof query.per_page !== 'number') {
-			throw new TypeError('[@cordxapp/instatus:error]: page param should be a valid number');
-		}
-		else if (typeof query !== 'object') {
-			throw new TypeError(
-				'[@cordxapp/instatus:error]: query should be a valid object | Ex: getQueriedIncidents({ page, per_page })'
-			);
-		}
+        if (!query || query == undefined) {
+            throw new ReferenceError('[@cordxapp/instatus:error]: please provide a valid query')
+        } else if (typeof query.page !== 'number') {
+            throw new TypeError('[@cordxapp/instatus:error]: page param should be a valid number')
+        } else if (typeof query.per_page !== 'number') {
+            throw new TypeError('[@cordxapp/instatus:error]: page param should be a valid number')
+        } else if (typeof query !== 'object') {
+            throw new TypeError(
+                '[@cordxapp/instatus:error]: query should be a valid object | Ex: getQueriedIncidents({ page, per_page })'
+            )
+        }
 
-		const res = await this._httpClient.get(`/${this._pageId}/subscribers`);
+        const res = await this._httpClient.get(`/${this._pageId}/subscribers`)
 
-		if (res.data.error) throw new Error(`[@cordxapp/instatus:error]: ${res.data.error}`);
+        if (res.data.error) throw new Error(`[@cordxapp/instatus:error]: ${res.data.error}`)
 
-		return res.data;
-	}
+        return res.data
+    }
 }
