267bbf966d74339de0e53146c9fa843d2e18346e
TheRealToxicDev <toxic.dev09@gmail.com>
2024-12-10 19:19:33 -0700

    feat(update): yes yes, again

--- packages/lintydeps/src/index.ts
+++ packages/lintydeps/src/index.ts
@@ -5,32 +5,56 @@ import yaml from 'js-yaml'
 import { defaultConfig } from './config/default'
 import lintydeps from './client/lintydeps'
 import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
 
 const CWD = process.cwd()
-const argv = yargs.argv as { _: (string | number)[]; [key: string]: unknown }
+const argv = yargs(hideBin(process.argv)).argv as { _: (string | number)[]; [key: string]: unknown }
+
+const CONFIG_FILE_NAME = '.lintyrc'
+const CONFIG_NOT_FOUND_MESSAGE = `Config file \`${CONFIG_FILE_NAME}\` not found. Using default config.\nAdd \`${CONFIG_FILE_NAME}\` file in your repo root if you wish to specify custom logic.\n`
+
+interface Config {
+    [key: string]: unknown
+}
 
 function readFile(filePath: string): string {
-    return fs.readFileSync(filePath, 'utf8').replace(/^\ufeff/, '')
+    try {
+        return fs.readFileSync(filePath, 'utf8').replace(/^\ufeff/, '')
+    } catch (error) {
+        console.error(`Error reading file at ${filePath}:`, error)
+        throw error
+    }
 }
 
-let cliArguments = argv._
-if (cliArguments.length === 0) {
-    console.error("Please provide the start location as 'linty <location>'. E.g., linty .")
-} else {
-    const dir = path.resolve(CWD, cliArguments[0] as string)
-    const configFilePath = path.join(dir, '.ndcrc')
-    let userConfig = {}
-    let config = {}
-
+function loadConfig(configFilePath: string): Config {
     if (fs.existsSync(configFilePath) && fs.statSync(configFilePath).isFile()) {
-        console.log('Config file `.lintyrc` found.\n')
-        userConfig = yaml.load(stripComments(readFile(configFilePath))) || {}
+        console.log(`Config file \`${CONFIG_FILE_NAME}\` found.\n`)
+        try {
+            const fileContent = readFile(configFilePath)
+            return (yaml.load(stripComments(fileContent)) as Config) || {}
+        } catch (error) {
+            console.error(`Error parsing config file at ${configFilePath}:`, error)
+            throw error
+        }
     } else {
-        console.log(
-            "Config file `.lintyrc` not found. Using default config.\nAdd '.lintyrc' file in your repo root if you wish to specify custom logic.\n"
-        )
+        console.log(CONFIG_NOT_FOUND_MESSAGE)
+        return {}
+    }
+}
+
+function main() {
+    const cliArguments = argv._
+    if (cliArguments.length === 0) {
+        console.error("Please provide the start location as 'linty <location>'. E.g., linty .")
+        process.exit(1)
     }
 
-    config = { ...defaultConfig, ...userConfig }
-    lintydeps(dir, config as any)
+    const dir = path.resolve(CWD, cliArguments[0] as string)
+    const configFilePath = path.join(dir, CONFIG_FILE_NAME)
+    const userConfig = loadConfig(configFilePath)
+    const config = { ...defaultConfig, ...userConfig }
+
+    lintydeps(dir, config)
 }
+
+main()
--- packages/prettierlogs/src/index.ts
+++ packages/prettierlogs/src/index.ts
@@ -1,5 +1,4 @@
-export { prettier } from './client/prettier'
-export * as Typings from './typings/index'
-export * as Constants from './helpers/constants'
-export * as Timestamp from './helpers/getTimestamp'
-export * as ShouldLog from './helpers/shouldLog'
+import { prettier } from './client/prettier'
+import { LogLevel, LogFile } from './typings/index'
+
+export { prettier, LogLevel, LogFile }
